46da37644706d270aa9d179c72e12f35948854d3
JonyBarroso <jony.barroso.2@gmail.com>
2022-09-10 00:49:58 -0300

    correccion cuarto desafio

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,36 +1,37 @@
 import React from "react";
 import Carrito from "../../src/assets/carrito.png"
+import athena from "../components/imagenes/athena.jpg"
 
 const NavBar = () => {
     return (
 
     <div className="container">
-        <nav class="col-sm-12 px-2 navbar navbar-expand-sm bg-light">
-    <div class="container-fluid">
-            <a class="navbar-brand" href="#">Inicio</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <nav className="col-sm-12 px-6 navbar navbar-expand-sm bg-light">
+    <div className="container-fluid">
+            <a className="navbar-brand" href="inicio"> <img src={athena} className="athena" /></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Nosotros</a>
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Nosotros</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contacto</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contacto</a>
         </li>
-        <form class="container-fluid justify-content-start">
-    <button class="btn btn-outline-success me-2" type="button"> <img src={Carrito} className="carrito" /> carrito</button>
+        <form className="container-fluid justify-content-start">
+    <button className="btn btn-outline-success me-2" type="button"> <img src={Carrito} className="carrito" /> carrito</button>
   </form>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle"  href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle"  href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Modelos
           </a>
           
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Mate Geometrico</a></li>
-            <li><a class="dropdown-item" href="#">Mates Peritas</a></li>
-            <li><a class="dropdown-item" href="#">Cazuelas</a></li>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Mate Geometrico</a></li>
+            <li><a className="dropdown-item" href="#">Mates Peritas</a></li>
+            <li><a className="dropdown-item" href="#">Cazuelas</a></li>
             
             
           </ul>
