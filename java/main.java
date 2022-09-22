d212252b3eb4d50f1ab25f64524b4a5829e475d4
JonyBarroso <jony.barroso.2@gmail.com>
2022-09-22 18:58:06 -0300

    primer desafio java

---
+++ java/main.java
@@ -0,0 +1,16 @@
+
+public class main{
+	
+	public static void main (String[] args) {
+		
+		
+		float numero = 18.53f;
+		
+		int parteEntera = 10;
+		double parteDecimal = 8.53f;
+
+		System.out.println("VALOR INICIAL : " +  numero);
+		System.out.println("VALOR DECIMAL : " +  parteDecimal);
+		System.out.println("VALOR ENTERA : " +  parteEntera);
+	}
+}
