0f720737f46343e768108d5a0285c7c94eb0c4fd
JonyBarroso <jony.barroso.2@gmail.com>
2022-11-28 17:34:20 -0300

    correccion carrito

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Item = ({item}) => {
     return (
         <div className="card fondo_amarillo border-0">
-            <Link to={item.id} className="text-decoration-none">
+            <Link to={"/item/" + item.id} className="text-decoration-none">
                 <img src={item.imagen} className="card-img-top" alt={item.nombre} />
                 <div className="card-body">
                     <h5 className="card-title text-center color_marron"><b>{item.nombre}</b></h5>
